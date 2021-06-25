2990e33f17568dc01f745d1f345866f86a330527
ZachLondono <zachary.londono5@gmail.com>
2021-06-24 22:16:14 -0400

    Working on network messaging

--- P2PNetworking/Message.cs
+++ P2PNetworking/Message.cs
@@ -1,10 +1,16 @@
+using System;
+using System.Net;
+using System.Net.Sockets;
 using System.Runtime.InteropServices;
 
 namespace P2PNetworking {
 
 	public enum MessageType : byte {
 		ProtocolCheck = 0,
-		KnwonPeers = 1
+		PeerRequest = 1,
+		RecordResponse = 2,
+		InvalidRequest = 3,
+		UnsuportedProtocolVersion = 4
 	}
 
 	public struct MessageHeader {
@@ -13,7 +19,7 @@ namespace P2PNetworking {
 		public int ContentLength;
 
 		/// Returns the bytes which represent the given header struct
-		public byte[] GetBytes(MessageHeader header) {
+		public static byte[] GetBytes(MessageHeader header) {
 			return new byte[0];
 		}
 
@@ -23,15 +29,46 @@ namespace P2PNetworking {
 		}
 	}
 
-	public struct KnownPeers {
-		public byte Count;
-		public Peer[] peers;
-	}
-
 	public struct Peer {
 		[MarshalAs(UnmanagedType.ByValTStr, SizeConst = 255)]
 		public string host;
 		public ushort port;
 	}
 
+	public class ArrayContent<T> {
+		private byte _count;
+		private T[] _content; 
+		public byte Count { get => _count; }
+		public T[] Contents {
+			get => _content;
+			set {
+				_content = value;
+				_count = Convert.ToByte(value.Length);
+			}
+		}
+
+		public byte[] GetBytes() {
+			
+			int objSize = Marshal.SizeOf(typeof(T));
+			
+			byte[] encoded = new byte[objSize * Count];
+
+			for (int i = 0; i < Count; i++) {
+				
+				int offset = objSize * i;
+
+				T obj = Contents[i];				
+
+				IntPtr ptr = Marshal.AllocHGlobal(objSize);
+				Marshal.StructureToPtr(obj, ptr, true);
+				Marshal.Copy(ptr, encoded, offset, objSize);
+				Marshal.FreeHGlobal(ptr);
+
+			}
+
+			return encoded;
+			
+		}
+
+	}
 }
