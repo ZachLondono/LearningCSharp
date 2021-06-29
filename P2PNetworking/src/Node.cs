4de9d81f7fcaeed8fac7d4c0ae4a64cadcb63344
ZachLondono <zachary.londono5@gmail.com>
2021-06-29 18:48:14 -0400

    Fixing formatting

--- P2PNetworking/src/Node.cs
+++ P2PNetworking/src/Node.cs
@@ -46,52 +46,47 @@ namespace P2PNetworking {
         private readonly int Port;
         private readonly int Backlog;
 
-        public Node() : this(11000, 10) {            
-            // Start on default port
-        }
+	public Node() : this(11000, 10) {            
+		// Start on default port
+	}
 
         public Node(int port, int backlog) {
- 
-                
- 			Port = port;
-            Backlog = backlog;
-            
-            Console.WriteLine("Starting Peer...");
-
-            // List to store connections
-            Connections = new List<ClientHandler>();
-            
-            Console.WriteLine("Opening Connection To Database...");
-            // Open new connection to database
-            DBConnection = new SqliteConnection("Data Source=Peer.db");
-            DBConnection.Open();
-
-            // If the peers table does not exist (first time stating this peer), create it
-            var command = DBConnection.CreateCommand();
-            command.CommandText = 
-            @"
-                CREATE TABLE IF NOT EXISTS peers (
-                    host VARCHAR(100),
-                    port INTEGER
-                );
-            ";
-            Console.WriteLine("Creating peers Table if it does not yet exist");
-            command.ExecuteNonQuery();
-
-            MessageMap = new Dictionary<MessageType, OnMessageTypeRecieved>();
-            
-            MessageMap.Add(MessageType.CONNECT, 
-					delegate(ClientHandler source, MessageReceivedArgs args) {
-                
-				Console.WriteLine("Connection check message recieved");
-                source.SendMessage(MessageType.REQUEST_SUCCESSFUL, null, false);
-
-            });
-            
-	    	MessageMap.Add(MessageType.REQUEST_SUCCESSFUL, 
-					delegate(ClientHandler source, MessageReceivedArgs args) {
-				Console.WriteLine("Successful Connection");
-            });
+		
+		Port = port;
+		Backlog = backlog;
+		
+		Console.WriteLine("Starting Peer...");
+		
+		// List to store connections
+		Connections = new List<ClientHandler>();
+		
+		Console.WriteLine("Opening Connection To Database...");
+		// Open new connection to database
+		DBConnection = new SqliteConnection("Data Source=Peer.db");
+		DBConnection.Open();
+		
+		// If the peers table does not exist (first time stating this peer), create it
+		var command = DBConnection.CreateCommand();
+		command.CommandText = 
+		@"
+		    CREATE TABLE IF NOT EXISTS peers (
+		        host VARCHAR(100),
+		        port INTEGER
+		    );
+		";
+		Console.WriteLine("Creating peers Table if it does not yet exist");
+		command.ExecuteNonQuery();
+		
+		MessageMap = new Dictionary<MessageType, OnMessageTypeRecieved>();
+		
+		MessageMap.Add(MessageType.CONNECT, delegate(ClientHandler source, MessageReceivedArgs args) {
+			Console.WriteLine("Connection check message recieved");
+			source.SendMessage(MessageType.REQUEST_SUCCESSFUL, null, false);
+		});
+		
+		MessageMap.Add(MessageType.REQUEST_SUCCESSFUL, delegate(ClientHandler source, MessageReceivedArgs args) {
+			Console.WriteLine("Successful Connection");
+		});
 
         }
 
@@ -105,73 +100,68 @@ namespace P2PNetworking {
 		Socket listener  = new Socket(iPAddress.AddressFamily, SocketType.Stream, 
 		ProtocolType.Tcp);
 
-            try {
+		try {
 
-                Console.WriteLine($"Listening for connections on: {Dns.GetHostName()}:{Port}");
+                	Console.WriteLine($"Listening for connections on: {Dns.GetHostName()}:{Port}");
 
-                listener.Bind(localEndPoint);
-                listener.Listen(Backlog);
+                	listener.Bind(localEndPoint);
+                	listener.Listen(Backlog);
 
-                while (true) {
+                	while (true) {
 
-                    Socket handler = listener.Accept();
+                		Socket handler = listener.Accept();
+				AddPeer(handler);
 
-                    AddPeer(handler);
+                	}
 
-                }
-
-            } catch (Exception e) {
-
-                Console.Write("Unable to listen for connections " + e.ToString());
-
-            }
+		} catch (Exception e) {
+			Console.Write("Unable to listen for connections " + e.ToString());
+		}
 
         }
 
         /// Connects to all known peers stored in local database
         public void ConnectToPeers() {
             
-            Console.WriteLine("Attempting to Connect to Known Peers");
+		Console.WriteLine("Attempting to Connect to Known Peers");
+		
+		// Read all peer entries from table
+		var command = DBConnection.CreateCommand();
+		command.CommandText = 
+		@"
+		    SELECT host, port FROM peers;
+		";
+		
+		
+		using (var reader = command.ExecuteReader()) {
+		
+			while (reader.Read()) {                        
+				var host = reader.GetString(0);
+				var port = reader.GetInt32(1);
+				Console.WriteLine("Attempting to Connect to Peer: {0}:{1}", host, port);
+				
+				try {
+					
+					// Connect to new peer
+					ClientHandler handler = AddPeer(host, port);
+					Console.WriteLine("Successful Connection to Peer: {0}:{1}", host, port);
+					
+					handler.SendMessage(MessageType.CONNECT, null, false);
+				
+				} catch (Exception e) {
+					// Error connecting to new peer, remove it from peers list
+					command = DBConnection.CreateCommand();
+					command.CommandText = "DELETE FROM peers WHERE host = $host AND port = $port;";
+					command.Parameters.AddWithValue("$host", host);
+					command.Parameters.AddWithValue("$port", port);
+					
+					int row = command.ExecuteNonQuery();
+					Console.WriteLine($"Removed peer {host}:{port} for failing to connect");
+				}
 
-            // Read all peer entries from table
-            var command = DBConnection.CreateCommand();
-            command.CommandText = 
-            @"
-                SELECT host, port FROM peers;
-            ";
+			}
 
-
-            using (var reader = command.ExecuteReader()) {
-
-                while (reader.Read()) {                        
-                    var host = reader.GetString(0);
-                    var port = reader.GetInt32(1);
-                    Console.WriteLine("Attempting to Connect to Peer: {0}:{1}", host, port);
-
-                    try {
-                        
-                        // Connect to new peer
-                        ClientHandler handler = AddPeer(host, port);
-                        Console.WriteLine("Successful Connection to Peer: {0}:{1}", host, port);
-
-                        handler.SendMessage(MessageType.CONNECT, null, false);
-
-                    } catch (Exception e) {
-                        // Error connecting to new peer, remove it from peers list
-
-                        command = DBConnection.CreateCommand();
-                        command.CommandText = "DELETE FROM peers WHERE host = $host AND port = $port;";
-                        command.Parameters.AddWithValue("$host", host);
-                        command.Parameters.AddWithValue("$port", port);
-
-                        int row = command.ExecuteNonQuery();
-                        Console.WriteLine($"Removed peer {host}:{port} for failing to connect");
-
-                    }
-
-                }
-
-            }
+		}
         }
 
         public void OnMessageReceived(object source, MessageReceivedArgs message) {
@@ -207,68 +197,67 @@ namespace P2PNetworking {
 
         public ClientHandler AddPeer(Socket socket) {
 
-            // Start thread to handle communications with this socket
-            ClientHandler handler = new ClientHandler(socket);
-            // Handler called on message receive
-            handler.MessageReceived += OnMessageReceived;
-            // Handler called on peer disconnect
-            handler.PeerDisconected += delegate (object o, EventArgs e) {
-                Console.WriteLine("Removing Stale Connection");
-                Connections.Remove((ClientHandler) o);
-            };
+		// Start thread to handle communications with this socket
+		ClientHandler handler = new ClientHandler(socket);
+		// Handler called on message receive
+		handler.MessageReceived += OnMessageReceived;
+		// Handler called on peer disconnect
+		handler.PeerDisconected += delegate (object o, EventArgs e) {
+		    Console.WriteLine("Removing Stale Connection");
+		    Connections.Remove((ClientHandler) o);
+		};
+		
+		Thread handlerThread = new Thread(new ThreadStart(handler.Run));
+		handlerThread.Start();
+		
+		Connections.Add(handler);
+		
+		return handler;
+	}
 
-            Thread handlerThread = new Thread(new ThreadStart(handler.Run));
-            handlerThread.Start();
-
-            Connections.Add(handler);
-
-            return handler;
-
-        }
-
-        public ClientHandler AddPeer(string host, int port) {
+	public ClientHandler AddPeer(string host, int port) {
             
-            // Get information about the host
-            IPHostEntry ipHostInfo = Dns.GetHostEntry(host); 
-            IPAddress ipAddress = ipHostInfo.AddressList[0];  
-            IPEndPoint remoteEP = new IPEndPoint(ipAddress,port);  
+		// Get information about the host
+		IPHostEntry ipHostInfo = Dns.GetHostEntry(host); 
+		IPAddress ipAddress = ipHostInfo.AddressList[0];  
+		IPEndPoint remoteEP = new IPEndPoint(ipAddress,port);  
+		
+		// Create a socket to connect to remote host
+		Socket peerSocket = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+		
+		// Attempt to connect
+		peerSocket.Connect(remoteEP);
+		
+		// If no exception is thrown, add connection to list of connections
+		return AddPeer(peerSocket);
 
-            // Create a socket to connect to remote host
-            Socket peerSocket = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+	}
 
-            // Attempt to connect
-            peerSocket.Connect(remoteEP);
+	public void SavePeer(string host, int port) {
 
-            // If no exception is thrown, add connection to list of connections
-            return AddPeer(peerSocket);
-        
-        }
+		// store host and port in database, if they do not already exist
+		SqliteCommand check = DBConnection.CreateCommand();
+		
+		// Check if peer already exists in table
+		check.CommandText = "SELECT 1 FROM peers WHERE host = $host AND port = $port;";
+		    check.Parameters.AddWithValue("$host", host);
+		check.Parameters.AddWithValue("$port", port);
+		
+		using (var reader = check.ExecuteReader()) {
+		    // If any rows are returned, it exists
+		    if (reader.HasRows) return;
+		}
+		
+		// Insert new peer into table
+		SqliteCommand insert = DBConnection.CreateCommand();
+		insert.CommandText = "INSERT INTO peers (host, port) VALUES ($host, $port);";
+		    insert.Parameters.AddWithValue("$host", host);
+		insert.Parameters.AddWithValue("$port", port);
+		
+		int rows = insert.ExecuteNonQuery();
+		if (rows < 1) Console.Write("Error saving new peer");
 
-        public void SavePeer(string host, int port) {
-
-            // store host and port in database, if they do not already exist
-            SqliteCommand check = DBConnection.CreateCommand();
-
-            // Check if peer already exists in table
-            check.CommandText = "SELECT 1 FROM peers WHERE host = $host AND port = $port;";
-	        check.Parameters.AddWithValue("$host", host);
-            check.Parameters.AddWithValue("$port", port);
-
-            using (var reader = check.ExecuteReader()) {
-                // If any rows are returned, it exists
-                if (reader.HasRows) return;
-            }
-
-            // Insert new peer into table
-            SqliteCommand insert = DBConnection.CreateCommand();
-            insert.CommandText = "INSERT INTO peers (host, port) VALUES ($host, $port);";
-	        insert.Parameters.AddWithValue("$host", host);
-            insert.Parameters.AddWithValue("$port", port);
-
-            int rows = insert.ExecuteNonQuery();
-            if (rows < 1) Console.Write("Error saving new peer");
-
-        }
+	}
 
     }
 
