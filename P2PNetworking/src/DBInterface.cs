4283130771273ba30798139c5a2a1188418cdff8
ZachLondono <zachary.londono5@gmail.com>
2021-07-23 23:45:13 -0400

    Implementing database interface

--- P2PNetworking/src/DBInterface.cs
+++ P2PNetworking/src/DBInterface.cs
@@ -1,6 +1,23 @@
 using Microsoft.Data.Sqlite;
+using System.Collections.Generic;
 
 namespace P2PNetworking {
+
+	public struct Data {
+		byte[] key;
+		byte[] value;
+	}
+
+	public struct Peer {
+		public string Host { get; }
+		public int Port { get; }
+
+		public Peer(string host, int port) {
+			Host = host;
+			Port = port;
+		}
+
+	}
 
 	public class DBInterface {
 	
@@ -34,33 +51,101 @@ namespace P2PNetworking {
 
 		}
 
-		public bool ContainsKey(byte key) {
-			return false;			
+		public bool ContainsKey(byte[] key) {
+			
+			var command = DBConnection.CreateCommand();
+			command.CommandText = "SELECT * WHERE key = $key";
+			command.Parameters.Add("$key", SqliteType.Blob, key.Length).Value = key;
+			
+			var reader = command.ExecuteReader();
+
+			return reader.HasRows;
+
 		}
 
-		public void InsertPair(byte key, byte value) {
+		public bool InsertPair(byte[] key, byte[] value) {
+
 			// Inserts the key value pair, there should only be one instance of key
+			// NOTE: this is likely susceptible to duplicate keys in the case of a race condition
+
+			var command = DBConnection.CreateCommand();
+			command.CommandText = @"BEGIN
+									IF NOT EXISTS (SELECT * FROM data WHERE key = $key)
+									BEGIN
+										INSERT INTO data (key, value) VALUES ( $key, $value )
+										END
+									END;";
+			command.Parameters.Add("$key", SqliteType.Blob, key.Length).Value = key;
+			command.Parameters.Add("$value", SqliteType.Blob, value.Length).Value = value;
+			
+			// insertion was successful if 1 row was changed
+			int rows = command.ExecuteNonQuery();
+			if (rows != 1) 
+				Node.WriteLine($"ERROR inserting pair. Row altered: {rows}");
+
+			return rows == 1;
+
 		}
 
 		public byte[] SelectData(string dataCol, string conditionCol, byte[] conditionVal) {
 	
 			// Returns data from dataCol where conditionCol = ConditionVal
 			var command = DBConnection.CreateCommand();
-			command.CommandText = "SELECT $dataCol FROM data WHERE $conditionCol = $conditionalVal";
+			command.CommandText = "SELECT $dataCol FROM data WHERE $conditionCol = $conditionalVal;";
 			command.Parameters.AddWithValue("$dataCol",dataCol);
 			command.Parameters.AddWithValue("$conditionCol",conditionCol);
 			command.Parameters.Add("$conditionVal", SqliteType.Blob, conditionVal.Length).Value = conditionVal;
 
+			using (var reader = command.ExecuteReader()) {
+				// READ BYTES
+			}
+
 			return null;
 		}	
 
-		// public Peer[] GetPeers() {}
+		public List<Peer> GetPeers() {
+			
+			List<Peer> peers = new List<Peer>(); 
 
-		// public void InsertPeer() {}
+			var command = DBConnection.CreateCommand();
+			command.CommandText = "SELECT host, port FROM peers;";
 
-		// public void RemovePeer() {}
+			using (var reader = command.ExecuteReader()) {
+				while (reader.Read()) {
+					var host = reader.GetString(0);
+					var port = reader.GetInt32(1);
+					Peer peer = new Peer(host, port);
+					peers.Add(peer);
+				}
+			}
 
-		// public void BlackListPeer() {} 
+			return peers;
+
+		}
+
+		public bool InsertPeer(Peer newPeer) {
+
+			var command = DBConnection.CreateCommand();
+			command.CommandText = "INSERT INTO peers (host, port) VALUES ( $host, $port);";
+			command.Parameters.AddWithValue("$host", newPeer.Host);
+			command.Parameters.AddWithValue("$port", newPeer.Port);
+
+			return 1 == command.ExecuteNonQuery();
+
+		}
+
+		public bool RemovePeer(Peer peer) {
+
+			var command = DBConnection.CreateCommand();
+			command.CommandText = "DELETE FROM peers WHERE host = $host AND port = $port;";
+			command.Parameters.AddWithValue("$host", peer.Host);
+			command.Parameters.AddWithValue("$port", peer.Port);
+
+			return 1 == command.ExecuteNonQuery();
+
+		}
+
+		// public void BlackListPeer(Peer badPeer) { } 
 
 	}
 
